f42c9533dc428f7e40f59db67745238fe0f08b84
Monika Junik <monica.junik@gmail.com>
2022-03-09 09:14:42 +0100

    fixed issue with folder uppercase

--- src/Layouts/Header.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-test("should display menu item", () => {});
-
-test("should direct to a path after click", () => {});
-
-test("should display a logo", () => {});
